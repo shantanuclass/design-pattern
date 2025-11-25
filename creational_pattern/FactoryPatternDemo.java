942748d3037be9fc63b867517e0d6e47e331186a
shantanuclass <anantwarsr@rknec.edu>
2025-11-25 23:27:25 +0530

    Update FactoryPatternDemo.java

--- creational_pattern/FactoryPatternDemo.java
+++ creational_pattern/FactoryPatternDemo.java
@@ -1,33 +1,54 @@
-// Product interface
-interface Toy {
-    void play();
-}
+public class Main {
 
-// Concrete Products
-class Car implements Toy {
-    public void play() { System.out.println("Playing with a car!"); }
-}
-
-class Teddy implements Toy {
-    public void play() { System.out.println("Hugging the teddy!"); }
-}
-
-// Factory
-class ToyFactory {
-    static Toy getToy(String type) {
-        if (type.equalsIgnoreCase("car")) return new Car();
-        else if (type.equalsIgnoreCase("teddy")) return new Teddy();
-        return null;
+    // ----- Product Interface -----
+    interface IceCream {
+        void serve();
     }
-}
 
-// Client
-public class FactoryPatternDemo {
+    // ----- Concrete Products -----
+    static class ChocolateIceCream implements IceCream {
+        public void serve() {
+            System.out.println("Serving Chocolate Ice Cream 🍫");
+        }
+    }
+
+    static class VanillaIceCream implements IceCream {
+        public void serve() {
+            System.out.println("Serving Vanilla Ice Cream 🍦");
+        }
+    }
+
+    static class StrawberryIceCream implements IceCream {
+        public void serve() {
+            System.out.println("Serving Strawberry Ice Cream 🍓");
+        }
+    }
+
+    // ----- Factory Class -----
+    static class IceCreamFactory {
+        public static IceCream makeIceCream(String type) {
+            switch (type.toLowerCase()) {
+                case "chocolate":
+                    return new ChocolateIceCream();
+                case "vanilla":
+                    return new VanillaIceCream();
+                case "strawberry":
+                    return new StrawberryIceCream();
+                default:
+                    throw new IllegalArgumentException("Unknown ice cream type: " + type);
+            }
+        }
+    }
+
+    // ----- Main Method -----
     public static void main(String[] args) {
-        Toy toy1 = ToyFactory.getToy("car");
-        toy1.play();
 
-        Toy toy2 = ToyFactory.getToy("teddy");
-        toy2.play();
+        IceCream ice1 = IceCreamFactory.makeIceCream("chocolate");
+        IceCream ice2 = IceCreamFactory.makeIceCream("vanilla");
+        IceCream ice3 = IceCreamFactory.makeIceCream("strawberry");
+
+        ice1.serve();
+        ice2.serve();
+        ice3.serve();
     }
 }
